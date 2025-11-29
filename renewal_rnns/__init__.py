686a2b9b8266c98459266ec768815a5c6d8f1a80
Chidambaram Aditya Somasundaram <112239625+Raspberryfruitcake@users.noreply.github.com>
2025-11-30 04:26:33 +0530

    Create __init__.py

---
+++ renewal_rnns/__init__.py
@@ -0,0 +1,8 @@
+"""
+uniform-renewal-rnns
+
+Package for training GRU and vanilla RNN models on uniform renewal processes
+and evaluating them via true and empirical KL divergence.
+"""
+
+from . import processes, data, models, training, evaluation, analysis
